3fe50be4ddb1e4235b435c266652338bf62ffb72
Andy Teucher <andy.teucher@gmail.com>
2019-07-02 14:14:26 -0700

    Documentation

--- R/process_rank.R
+++ R/process_rank.R
@@ -10,17 +10,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#' Convert S ranks to numeric
+#' Convert NatureServe S ranks to numeric
 #'
-#' Converts an rank with S to a single number
+#' Converts NatureServ S ranks to a number from `0` (`"SX"`) to `5` (`"S5"`).
+#' Historic (`"SH"`) is converted to `0.5` (i.e., halfway between `0` and `1`).
+#' When there are range ranks they are converted to a numeric vector (e.g.,
+#' `"S2S4"` becomes `c(2,3,4)`)
 #'
 #' @param ranks character vector of input S ranks
-#' @param simplify converts list to numeric vector. Default = FALSE
-#' @param round for range ranks which are choose. Default = `'middle'`.
-#' Ignored if simplify is FALSE.
+#' @param simplify converts list to numeric vector, and rounds range ranks
+#' using the function supplied to `round_fun`. Default = FALSE
+#' @param round_fun what function to use (default `median`) to round range ranks into a single
+#' value when `simplify` is `TRUE`. Ignored if simplify is `FALSE`.
 #'
-#' @return a list the same length as `ranks` of numeric vectors. For range ranks,
-#'  the vector will be sequence from low to high (e.g.,`"S3S5"` becomes `c(3,4,5)`)
+#' @return a list the same length as `ranks` of numeric vectors. For range
+#'   ranks, the vector will be sequence from low to high (e.g.,`"S3S5"` becomes
+#'   `c(3,4,5)`)
 #'
 #' @export
 #'
@@ -31,14 +36,18 @@ ranks_to_numeric <- function(ranks, simplify = FALSE,
   ## Add argument checks
   single_ranks <- make_single_ranks(ranks)
 
+  # Remove S/N/G etc from the beginning and end
   numeric_1 <- gsub("^[^0-9XH]|[^0-9XH]+$", "", single_ranks)
+
   # Convert SX and SH to 0 and 0.5 respectively
   numeric_2 <- gsub("X", "0", numeric_1)
   numeric_3 <- gsub("H", "0.5", numeric_2)
   numeric_3[!nzchar(numeric_3)] <- NA_character_
-  # Split compound ranks into vectors and conver to numeric
+
+  # Split compound/range ranks into character vectors (split on any letters)
   char_list <- strsplit(numeric_3, "[a-zA-Z]")
-  # Create numeric vectors
+
+  # Create numeric vectors from character
   num_list <- lapply(char_list, function(x) {
     x <- as.numeric(x)
     # If just one rank or two adjacent ranks, leave it
@@ -51,8 +60,10 @@ ranks_to_numeric <- function(ranks, simplify = FALSE,
   })
 
   if (simplify) {
+    # find range ranks (vectors longer than 1)
     longer_than_one <- purrr::map_lgl(num_list, ~ length(.x) > 1)
     if (any(longer_than_one)) {
+      # round using provided round_fun
       rounded <- purrr::map_int(num_list[longer_than_one], round_fun)
       num_list[longer_than_one] <- rounded
     }
@@ -67,7 +78,7 @@ ranks_to_numeric <- function(ranks, simplify = FALSE,
 #' @importFrom purrr map_chr
 #' @noRd
 make_single_ranks <- function(ranks) {
-  # split ranks on commas
+  # split ranks on commas (with or without a space)
   ranks_split <- strsplit(ranks, ",\\s?")
   # when double-barrelled, choose the breeding rank
   map_chr(ranks_split, ~ {
