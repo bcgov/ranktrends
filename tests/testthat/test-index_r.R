57ca6b325801e5910604bb8e258209c0df52d063
Perkins <genevieve.perkins@gov.bc.ca>
2019-07-10 16:05:39 -0700

    Add rli function checks

--- tests/testthat/test-index_r.R
+++ tests/testthat/test-index_r.R
@@ -1,9 +1,11 @@
 
 context("Test the rli function")
 
-
 test_that("test the function works", {
   expect_is(rli(c(0,2,3)), "numeric")
+  expect_equal(rli(c(0,2,3,3)), 0.6)
+  expect_error(rli(c(6,2)),
+      "Numeric rank must be below the maxiumum weighted score for extinct species")
   })
 
 
@@ -11,10 +13,9 @@ test_that("test fails on non character input", {
   expect_error(rli('S1'),"status scores should be a numeric vector")
 })
 
-# this one is not working correctly
+
 test_that("test for NA data input", {
-  expect_equal(rli(NA_character_), "numeric" )
-  expect_warning(rli(NA_character_),
+  expect_error(rli(NA_real_),
                  "It looks like you have one or more NA values in your input dataset")
 })
 
