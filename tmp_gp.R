7dbf7f871d4ccfca5aaadd8dad44ed89ec3ad34d
Perkins <genevieve.perkins@gov.bc.ca>
2019-07-15 16:23:39 -0700

    Testing script with real data

--- tmp_gp.R
+++ tmp_gp.R
@@ -1,23 +1,63 @@
 library(tidyverse)
 library(furrr)
 
+
 # https://gist.github.com/ateucher/fc12bebfc189ab39b310d5321307475e
 # Base on Andy T's gist:
 
 status_data <- read_csv("https://catalogue.data.gov.bc.ca/dataset/4484d4cd-3219-4e18-9a2d-4766fe25a36e/resource/842bcf0f-acd2-4587-a6df-843ba33ec271/download/historicalranksvertebrates1992-2012.csv")
 
-head(status_data)
 
-tdata <- status_data[1:5,]
+# issues to fix:
+# non-breeding / breeding section needs fixing
+    # remove non-ranking species : ?,  SNA?, and  Unranked (U)
+    # add paramter to select B or N breeding ranks_to_numeric ie: breeding
+    # how to deal with SZN ?
 
-tdata$SRank
+     "S3S4B,SZN" "S5B" "S4B"  "S4S5" "S3S4" "S4?"  "S4S5B"
+[13] "S3B, S4N"  "S4?B"      "SU"     "S3B,S4N"
+   "S3N,S4B"   "S2B"       "S4?B,SZN"  "S5B,S5N"   "S3B,SZN"
+[25] "S2S3B,S4N" "S1B"       "S4S5B,S4N" "SNA"       "S2S3B"     "S2S3"
+[31] "S1S2B"     "S4B,S4N"   "S3N"       "SHN"       "S1N"
+[37] "S2M"       "SH"        "S2B,S3N"   "SNR"           "S3?"
+[43] "S4N"       "S1S2N"     "S1B,SZN"   "S1B,S4N"   "S2B,SZN"   "S4B,SZN"
+[49] "S4N,S5B"   "S3?B,SZN"  "S3S4N,SZN" "S2N,S3B"   "S1B,S3N"   "S5B,SZN"
+[55] "S5B,S4N"   "S4S5B,SZN" "S2B,S4N"   "S3B,S5N"   "S4B,S3N"   "S3B,S2N"
+[61] "S4S5B,S5N" "S4B,S5N"   "S1B,S2N"       "S3B,S3N"   "S5N"
+[67] "SHB,S4N"   "SXB, SNAN" "S2?B"
 
-ranks_to_numeric(tdata$SRank)
 
-status_data_wts <- tdata %>%
-  mutate(parsed_rank = ranks_to_numeric(SRank,simplify = TRUE),
-         wts = map(parsed_rank, ~ 5 - .x))
+# add unlist error check in rli function
 
+list.to.fix <- c("SNA", "SU" , "SNR")
+
+tdata <- status_data %>%
+   filter(Taxonomic_Group == "Mammals") %>%
+   filter(!SRank %in% list.to.fix)
+
+
+# working
+status_data<- tdata %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank, simplify = FALSE))
+
+# not working
+status_data<- tdata %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank, simplify = TRUE))
+# getting error with double to interger # line 86?
+#https://stackoverflow.com/questions/55397509/purrrmap-int-cant-coerce-element-1-from-a-double-to-a-integer
+
+unique(tdata$SRank)
+
+
+
+# select the subset of interest:
+
+# 1) group by taxonomic group
+
+status_data_wts <- status_data %>%
+  mutate(parsed_rank = unlist(ranks_to_numeric(SRank)),
+         wts = map(parsed_rank, ~ 5 - .x)) %>%
+  mutate(valid_rank = map_lgl(wts, ~ !all(is.na(.x))))
 
 status_complete <- status_data_wts %>%
   group_by(Taxonomic_Group) %>%
