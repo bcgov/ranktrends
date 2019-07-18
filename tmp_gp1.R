e2b327f9c7d62fce51264334e251da3e507d3d3c
Perkins <genevieve.perkins@gov.bc.ca>
2019-07-18 13:54:36 -0700

    add working temp file

---
+++ tmp_gp1.R
@@ -0,0 +1,114 @@
+library(tidyverse)
+library(furrr)
+
+
+# https://gist.github.com/ateucher/fc12bebfc189ab39b310d5321307475e
+# Base on Andy T's gist:
+
+status_data <- read_csv("https://catalogue.data.gov.bc.ca/dataset/4484d4cd-3219-4e18-9a2d-4766fe25a36e/resource/842bcf0f-acd2-4587-a6df-843ba33ec271/download/historicalranksvertebrates1992-2012.csv")
+
+
+# issues to fix:
+# 1) non-breeding / breeding section needs fixing
+    # remove non-ranking species : ?,  SNA?, and  Unranked (U)
+    # add paramter to select B or N breeding ranks_to_numeric ie: breeding
+    # how to deal with SZN ?
+
+# 2)  unusual calls ( list.to.fix <- c("SNA", "SU" , "SNR"))
+list.to.fix <- c("SNA", "SU" , "SNR")
+
+tdata <- status_data %>%
+  # filter(Taxonomic_Group == "Mammals") %>%
+  filter(!SRank %in% list.to.fix)
+
+# 3) do we want the output of ranks_to_numeric to be a list? \
+# working
+status_data <- tdata %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank, simplify = FALSE))
+
+
+# not working
+status_data <- tdata %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank, simplify = TRUE))
+
+# 4) getting error with double to interger # line 86?
+#https://stackoverflow.com/questions/55397509/purrrmap-int-cant-coerce-element-1-from-a-double-to-a-integer
+
+status_data_wts <- status_data %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank),
+         parsed_rank_single = map_dbl(parsed_rank, min),  # tried to extract 1st value ? not working
+         wts = unlist(map(parsed_rank_single, ~ 5 - .x))) # need to extract the first value
+
+status_data_wts <- status_data %>%
+  mutate(parsed_rank = ranks_to_numeric(SRank),
+         parsed_rank_single = ranks_to_numeric(SRank, simplify = TRUE, round_fun = min),  # tried to extract 1st value ? not working
+         wts = 5 - parsed_rank_single)
+
+status_complete <- status_data_wts %>%
+  group_by(Taxonomic_Group) %>%
+  complete(nesting(Scientific_Name, Common_Name), Year) %>%
+  mutate(valid_rank = map_lgl(wts, ~ !all(is.na(.x)))) %>%
+  semi_join(
+    group_by(., Taxonomic_Group, Scientific_Name, Common_Name) %>%
+      summarize(all_complete = all(valid_rank)) %>%
+      filter(all_complete),
+    by = c("Taxonomic_Group", "Scientific_Name", "Common_Name"))
+
+species_to_remove <- status_data %>%
+  filter(Year == min(Year) & SRank == "SX") %>%
+  pull(Scientific_Name) %>%
+  unique()
+
+status_data_final <- status_complete  %>%
+  filter(!Scientific_Name %in% species_to_remove) %>%
+  ungroup() %>%
+  mutate(Taxonomic_Group = ifelse(
+    Taxonomic_Group %in% c("Amphibians", "Reptiles and Turtles"),
+    "Reptiles & Amphibians",
+    Taxonomic_Group))
+
+plan(multiprocess)
+
+mammals <- filter(status_data_final, Taxonomic_Group == "Mammals", Year == 1992) %>%
+  mutate(wts_list = map(parsed_rank, ~ 5 - .x))# Ask Andy about this
+
+rli(purrr::map_dbl(mammals$wts, sample, 1))
+
+mutate(mammals,
+  N = n(),
+  samples = map(wts_list, ~ replicate(100,
+               rli(map_dbl(.x, sample, 1)))),
+  mean_wt = map_dbl(samples, mean),
+  min_wt = map_dbl(samples, min),
+  max_wt = map_dbl(samples, max),
+  lci = map_dbl(samples, quantile, probs = 0.025),
+  uci = map_dbl(samples, quantile, probs = 0.975))
+
+
+
+csi <- group_by(status_data_final, Taxonomic_Group, Year) %>%
+  nest() %>%
+  mutate(
+    N = map_dbl(data, nrow),
+    samples = map(
+      data,
+      ~ replicate(100,
+                  rli(map_dbl(.x$wts, sample, 1)))
+    ),
+    mean_wt = map_dbl(samples, mean),
+    min_wt = map_dbl(samples, min),
+    max_wt = map_dbl(samples, max),
+    lci = map_dbl(samples, quantile, probs = 0.025),
+    uci = map_dbl(samples, quantile, probs = 0.975))
+
+ggplot(csi, aes(x = Year)) +
+  facet_wrap(~Taxonomic_Group) +
+  geom_ribbon(aes(ymin = lci, ymax = uci), fill = "grey80") +
+  geom_line(aes(y = mean_wt))
+
+# Pseudo-code for desired function
+# rank_data %>%
+#   group_by(tax_group, yr) %>%
+#   sampled_index(wts_list) # sampled_index doesn't exist yet
+#
+# tax_group | yr | mean_wt | min_wt | max_wt | lci | uci
