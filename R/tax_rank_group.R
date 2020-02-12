4e907be34ec7acc1c36137f0034e84a8eef83ab4
GPerkins <Genevieve.Perkins@gov.bc.ca>
2020-02-12 12:24:48 -0800

    Add documentation for sampled_index function

--- R/tax_rank_group.R
+++ R/tax_rank_group.R
@@ -11,35 +11,42 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 
+#' Function to automate the rank trends by taxanomic group
+#'
+#' This function generates confidence intervals for ranks by taxanomic groups by resampling
+#' species within a given group at each time point.
+#'
+#' @param wt_data input dataframe of species weighted ranks
+#' @param tax_group name of column that contains grouping variable e.g. "taxonomic group"
+#' @param wts_col name of column with numeric weight values (e.g. "wts_col")
+#' @param yr_col name of column that specifies assessment (e.g. "year")
+#' @param nreps number of times to subsample species weights (default = 1000)
+#'
+#' @return a dataframe containing mean, upper and lower confidence interval for samples
+#' conservation status index
+#' @export
+#'
+#'
+sampled_index <- function(wt_data, tax_group, wts_col, yr_col, nreps= 1000){
 
-# Pseudo-code for desired function
-# rank_data %>%
-#   group_by(tax_group, yr) %>%
-#   sampled_index(wts_list) # sampled_index doesn't exist yet
-#
-# tax_group | yr | mean_wt | min_wt | max_wt | lci | uci
-
-# function to automate the rank trends by taxanomic group
-
-sampled_index <- function(wt_data, tax_group, wts_col, yr_col){
-
-    wt_data <- wt_data %>%
+  wt_data <- wt_data %>%
     dplyr::mutate_(wts = wts_col)
 
   csi <- dplyr::group_by_(wt_data, tax_group, yr_col) %>%
     tidyr::nest() %>%
     dplyr::mutate(
       N = purrr::map_dbl(data, nrow),
+      # TODO : split rows into double or single ranks - with resample only for double ranks (ie 3,4 not sp with 1 wt)
       samples = purrr::map(
         data,
-        ~ replicate(10, rli(purrr::map_dbl(.x$wts, sample, 1)))
+        ~ replicate(nreps, rli(purrr::map_dbl(.x$wts, sample, 1)))
       ),
       mean_wt = purrr::map_dbl(samples, mean),
       min_wt = purrr::map_dbl(samples, min),
       max_wt = purrr::map_dbl(samples, max),
-      lci = purrr::map_dbl(samples, stats::quantile, probs = 0.025),
+      lci = purrr::map_dbl(samples, stats::quantile, probs = 0.025), # TODO AT: makes into params
       uci = purrr::map_dbl(samples, stats::quantile, probs = 0.975)
-      )
- csi
+    )
+  csi
 
- }
+}
