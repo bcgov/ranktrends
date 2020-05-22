9e825dfc4d85f359190c250e311168e7c04ca5c3
Andy Teucher <andy.teucher@gov.bc.ca>
2020-05-22 11:54:17 -0700

    Add/fix tests

--- tests/testthat/test-rank_conversion.R
+++ tests/testthat/test-rank_conversion.R
@@ -1,4 +1,3 @@
-
 context("Test the rank conversion function")
 
 test_that("test for NA data input", {
@@ -16,15 +15,23 @@ test_that("test the function works", {
                list(1, 0, c(2, 3, 4)))
 })
 
+test_that("the function works on all rank combinations", {
+  test_ranks <- readRDS("test_rank_conversion.rds")
+  expect_warning(
+    num_ranks <- ranks_to_numeric(test_ranks$rank),
+    "There is one or more NA values"
+  )
+  expect_equal(num_ranks, test_ranks$num_rank)
+})
 
 test_that("test the simplify function works", {
-  expect_equal(ranks_to_numeric(c("S1", "SX", "S2S4"), simplify = TRUE),
-               c(1, 0, 3))
-  expect_equal(ranks_to_numeric(c("S1", "SX", "S2S4"), simplify = TRUE,
-                                round_fun = min), c(1, 0, 2))
-  expect_equal(ranks_to_numeric(c("S1", "SX", "S2S4"), simplify = TRUE,
-                                round_fun = max), c(1, 0, 4))
-
+  test_ranks <- c("S1", "SX", "S2S4", "SH", "S2?")
+  expect_equal(ranks_to_numeric(test_ranks, simplify = TRUE),
+               c(1, 0, 3, 0.5, 2))
+  expect_equal(ranks_to_numeric(test_ranks, simplify = TRUE,
+                                round_fun = min), c(1, 0, 2, 0, 1))
+  expect_equal(ranks_to_numeric(test_ranks, simplify = TRUE,
+                                round_fun = max), c(1, 0, 4, 1, 3))
 })
 
 
