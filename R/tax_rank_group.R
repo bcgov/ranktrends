8ddc72fe92d5c2802a95aef33f41837e132727c8
GPerkins <Genevieve.Perkins@gov.bc.ca>
2020-05-27 13:03:15 -0700

    attempt to incorporate sample prob

--- R/tax_rank_group.R
+++ R/tax_rank_group.R
@@ -30,7 +30,9 @@
 #'
 sampled_index <- function(wt_data, tax_group, wts_col, yr_col, nreps= 1000){
 
-  wt_data <- dplyr::mutate_(wt_data, wts = wts_col)
+  # use the look up table to get numeric, create wts column from character col
+
+  wt_data <- dplyr::mutate_(wt_data, wts = wts_col) # add prob
 
   wt_data <- dplyr::group_by_(wt_data, tax_group, yr_col)
   wt_data <- tidyr::nest(wt_data)
@@ -40,7 +42,7 @@ sampled_index <- function(wt_data, tax_group, wts_col, yr_col, nreps= 1000){
       # add a probability (vector corresponding to probability or type ie S1, S2, S3, = c(0.1, 0.8, 0.1))
       samples = purrr::map(
         .data$data,
-        ~ replicate(nreps, rli(purrr::map_dbl(.x$wts, sample, 1)))
+        ~ replicate(nreps, rli(purrr::map_dbl(.x$probs, sample(0:5, 1)))
       ),
       mean_wt = purrr::map_dbl(.data$samples, mean),
       min_wt = purrr::map_dbl(.data$samples, min),
@@ -51,3 +53,56 @@ sampled_index <- function(wt_data, tax_group, wts_col, yr_col, nreps= 1000){
   csi
 
 }
+
+
+# # Gen;s attempt
+#
+# sampled_index <- function(wt_data, tax_group, wts_col, yr_col, nreps = 1000){
+#
+#   wt_data = status_data_final
+#   tax_group = "taxonomic_group"
+#   rank_col = "crank"
+#   yr_col = "year"
+#   nreps = 100
+#
+#
+#   # use the look up table to get numeric, create wts column from character col
+#
+#   wt_data <- dplyr::mutate_(wt_data, rank = rank_col)
+#   wt_data <- dplyr::left_join(wt_data, ranks_prob_key, by = c("rank" = "basic_rank"))
+#   wt_data <-  wt_data[!is.na( wt_data$rank), ]
+#
+#   wt_data$prob_vec <- lapply(seq_len(nrow(wt_data)), function(x) {
+#     as.numeric(t(wt_data[x, c("p_0", "p_1", "p_2", "p_3", "p_4", "p_5")]))
+#   })
+#
+#   wt_data$samples <-lapply(wt_data$prob_vec, function(x) {
+#     sample(5:0, 100, replace = TRUE, prob = x)
+#   })
+#
+#   wt_data$rli <-lapply(wt_data$samples, function(x) {
+#     rli(x)
+#   })
+#
+#
+#   wt_data <- dplyr::group_by_(wt_data, tax_group, yr_col)
+#   wt_data <- tidyr::nest(wt_data)
+#
+#   csi <- dplyr::mutate(wt_data,
+#                        N = purrr::map_dbl(.data$data, nrow),
+#                        samples = purrr::map(
+#                          .data$data,
+#                          ~ rli(.data$samples)
+#                        )
+#   )
+#   #     mean_wt = purrr::map_dbl(.data$samples, mean),
+#   #     min_wt = purrr::map_dbl(.data$samples, min),
+#   #     max_wt = purrr::map_dbl(.data$samples, max),
+#   #     lci = purrr::map_dbl(.data$samples, stats::quantile, probs = 0.025), # TODO AT: makes into params
+#   #     uci = purrr::map_dbl(.data$samples, stats::quantile, probs = 0.975)
+#   csi
+#
+# }
+#
+#
+#
